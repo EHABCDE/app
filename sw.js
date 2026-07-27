eb719bbed48defebdec86f74c2b228ddd3be6ced
EHABCDE <j.ottermann@erste-hilfe-abc.de>
2026-07-27 10:58:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'eh-abc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './logo.jpg'
+];
+
+// Dateien beim ersten Laden in den Speicher des Handys laden
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Bei Netzwerk-Ausfall die App direkt aus dem Speicher laden
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
