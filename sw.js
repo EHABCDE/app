16106a36138ef55da1cd55072733d10096940b9f
EHABCDE <j.ottermann@erste-hilfe-abc.de>
2026-08-16 15:36:24 +0200

    Ukrainisch komplettiert

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// WICHTIG: Version auf v49 erhöht, um das Update bei allen Nutzern zu erzwingen!
-const CACHE_NAME = 'eh-abc-v49';
+// WICHTIG: Version auf v52 erhöht, um das Update bei allen Nutzern zu erzwingen!
+const CACHE_NAME = 'eh-abc-v52';
 const ASSETS = [
   './',
   './index.html',
